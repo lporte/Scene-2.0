c74dacf9133a79adbcf7f98abe00436a1497d651
Lauryn Porte <laurynamandaporte@gmail.com>
2015-09-20 02:54:57 -0700

    Yeoman setup

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scene2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scene2App
+ */
+angular.module('scene2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
